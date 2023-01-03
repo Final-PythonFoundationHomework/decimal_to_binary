88c839ff66da82f4f463a48353ba19630ee7907c
ZARIFMAC <uzbzarif@gmail.com>
2023-01-03 11:36:48 +0500

    decimal to binary

---
+++ main.py
@@ -0,0 +1,15 @@
+def decimal_to_binary(number: int) -> str:
+    """
+    Convert a decimal number to its binary representation.
+
+    args:
+        number: The decimal number to convert.
+    returns:
+        The binary representation of the number as a string.
+    """
+    pass
+
+
+# Example
+print(decimal_to_binary(10))  # 1010
+print(decimal_to_binary(100))  # 1100100
